1fac251e303ee8656eed7a1e7e0550cbfdda6cb5
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-01 22:37:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,20 @@ class Triangle
   end
 
   def kind
+    validate_triangle
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || z == x
+      :isosceles
+    else
+      :scalene
+  end
 
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
   end
 end
 
