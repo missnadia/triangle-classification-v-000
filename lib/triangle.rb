3f46081fe83dc31f295c7abe57c5ab2a62b4fa01
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-01 22:26:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+
+  end
+end
+
+class TriangleError < StandardError
 end
